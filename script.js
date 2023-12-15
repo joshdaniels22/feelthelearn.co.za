619e0c3ba6381abb862887ddf330bc873651a931
Joshua Daniels <joshuadaniels537@gmail.com>
2023-12-15 12:13:51 +0200

    gallery incomplete

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 let slideIndex = 1;
+let startIndex = 1;
+let endIndex = 4;
+let numTutors = 5;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+  slideIndex += n
+  updateFrame(slideIndex);
+  showSlides(slideIndex);
+}
+
+function updateFrame() {
+if (slideIndex > numTutors || slideIndex < 0) {
+
+} else {
+  // let slides = document.getElementsByClassName("demo");
+  if (slideIndex > endIndex) {
+    endIndex += 1;
+    startIndex += 1;
+    // slides[startIndex - 1].style.display = "none";
+    // slides[endIndex].style.display = "block";
+
+  } else if (slideIndex < startIndex) {
+    endIndex -= 1;
+    startIndex -= 1;
+    // slides[endIndex + 1].style.display = "none";
+    // slides[startIndex].style.display = "block";
+  }
+}
+
+
 }
 
 function currentSlide(n) {
@@ -15,17 +42,31 @@ function showSlides(n) {
   let dots = document.getElementsByClassName("demo");
   let captionText = document.getElementById("caption");
   let bios = document.getElementsByClassName("bio");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+
   for (i = 0; i < slides.length; i++) {
+      dots[i].style.display = "none";
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    if (i >= startIndex -1 && i <= endIndex - 1) {
+      dots[i].style.display = "block";
+    } else {
+      dots[i].style.display = "none";
+    }
+  }
+
+
+  if (n > endIndex) {
+    slideIndex = startIndex;
+  }
+  if (n < startIndex) {
+    slideIndex = endIndex;
+  }
+  for (i = 0; i < endIndex; i++) {
     slides[i].style.display = "none";
     bios[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < endIndex; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex - 1].style.display = "block";
