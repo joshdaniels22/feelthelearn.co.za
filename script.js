2ed25f15cf5040f4ca74df1a608b5c5df6683ed7
Joshua Daniels <joshuadaniels@Joshuas-MacBook-Air.local>
2023-12-24 00:19:06 +0200

    feature: picture gallery click or drag to move

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showCaptionForSlide(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
+function showCaptionForSlide(i) {
+  slideIndex = i
   let bios = document.getElementsByClassName("bio");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  for (i = 0; i < bios.length; i++) {
     bios[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
   bios[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
 }
